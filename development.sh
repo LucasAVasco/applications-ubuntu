ea99fe68ffc37c31508a618930c97c141f71e5ab
LucasAVasco <none>
2020-10-22 16:50:46 -0300

    Initial 'development.sh'

---
+++ development.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Basics
+apt install -y vim perl tmux git build-essential universal-ctags
+
+# Neo Vim
+apt install -y neovim nodejs npm ccls
+
+# Search
+apt isntall -y fzf silversearcher-ag
+
+# Bat
+echo 'download Bat in the "https://github.com/sharkdp/bat/releases" site'
