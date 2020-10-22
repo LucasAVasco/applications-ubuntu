5ff0592c25e742ffe5d6877bf5b8bc241081c82a
LucasAVasco <none>
2020-10-22 16:48:41 -0300

    Add Microsoft fonts

--- fonts.sh
+++ fonts.sh
@@ -7,3 +7,6 @@ apt install -y fonts-noto-core fonts-noto-mono
 HOME_PATH=~
 mkdir -p ~/.local/share/fonts/hack
 curl --location https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf --output ${HOME_PATH}'/.local/share/fonts/hack/Hack Regular Nerd Font Complete Mono.ttf'
+
+# MS fonts
+apt install -y msttcorefonts
