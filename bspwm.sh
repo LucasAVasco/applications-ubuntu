7716e6af7506e64f5cc1f03b0c3dd49b5d393fd7
LucasAVasco <none>
2020-10-27 21:54:37 -0300

    Add 'obsession' installation
    Obsession add session management helpers. It is used for logout and
    shutdown.

--- bspwm.sh
+++ bspwm.sh
@@ -6,5 +6,8 @@ apt install -y bspwm sxhkd pulseaudio rofi
 # Customization
 apt install -y lxappearance nitrogen compton
 
+# Obsession
+#apt install -y obsession
+
 # Polybar
 echo 'you need to install Polybar for the Ubuntu groovy packages repository: https://packages.ubuntu.com/groovy/amd64/polybar/download'
