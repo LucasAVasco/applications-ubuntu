bb493c07352333f806a7e4b3904aff619e405006
LucasAVasco <none>
2021-02-02 18:44:00 -0300

    Add Godot installation

---
+++ game_development.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# OpenGL - Dev
+apt install libglu1-mesa-dev freeglut3-dev mesa-common-dev
+
+# OpenGL ES - Dev
+apt install libglfw3-dev libgles2-mesa-dev
+
+# Local Script
+echo 'Execute the another script'
---
+++ local/game_development.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Solve dependences
+echo 'Execute the another script'
+
+# Godot
+echo download godot engine in \"https://godotengine.org/download/linux\" and press \"Enter\"
+read
+mkdir -p ~/.local/etc/godot
+mv ~/Downloads/Godot_v* ~/.local/etc/godot/
+unzip ~/.local/etc/godot/Godot_v* -d ~/.local/etc/godot/
+rm ~/.local/etc/godot/Godot_v*.zip
+mv ~/.local/etc/godot/Godot_v* ~/.local/etc/godot/godot
