b2626b968a7207513829608d635e3d7229bcf725
LucasAVasco <none>
2021-02-02 18:28:04 -0300

    Add Obsession installation

--- bspwm.sh
+++ bspwm.sh
@@ -7,7 +7,7 @@ apt install -y bspwm sxhkd pulseaudio rofi
 apt install -y lxappearance nitrogen compton
 
 # Obsession
-#apt install -y obsession
+apt install -y obsession
 
 # Polybar
 echo 'you need to install Polybar for the Ubuntu groovy packages repository: https://packages.ubuntu.com/groovy/amd64/polybar/download'
