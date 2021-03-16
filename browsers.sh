0853faf5c44d67c1c7233c302bd05b83dcb5464a
LucasAVasco <none>
2021-03-16 14:41:34 -0300

    Fluent Reader Installation

--- browsers.sh
+++ browsers.sh
@@ -7,5 +7,10 @@ curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | apt-key 
 
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
 
-apt update
+apt update -y
 apt install -y brave-browser
+
+
+# Fluent Reader
+echo 'Download Fluent Reader in "https://github.com/yang991178/fluent-reader/releases" site'
+read
