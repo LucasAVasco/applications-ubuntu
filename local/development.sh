ba87cde8d62c9f36e25ada225c67b8df9f711cab
Lucas Vasco <lucas@pop-os.localdomain>
2021-03-31 15:58:17 -0300

    Add italic support to Neo-vim and Tmux

--- local/development.sh
+++ local/development.sh
@@ -9,3 +9,9 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 cd ~/.asdf
 git checkout "$(git describe --abbrev=0 --tags)"
 cd -
+
+
+# Italic to Neovim
+tic -o "$HOME/.terminfo" tmux.terminfo
+tic -o "$HOME/.terminfo" tmux-256color.terminfo
+tic -o "$HOME/.terminfo" xterm-256color.terminfo
