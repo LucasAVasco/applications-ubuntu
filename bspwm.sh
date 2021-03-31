06617e14c033f0bdcff028ccfd0e70a12ee46ac9
Lucas Vasco <lucas@pop-os.localdomain>
2021-03-31 14:58:39 -0300

    Slim installation
    Slim is used as screen locker.

--- bspwm.sh
+++ bspwm.sh
@@ -6,6 +6,9 @@ apt install -y bspwm sxhkd pulseaudio pavucontrol rofi brightnessctl
 # Customization
 apt install -y lxappearance nitrogen compton
 
+# Screen locker
+apt install -y slim
 
 # Polybar
 echo 'you need to install Polybar for the Ubuntu groovy packages repository: https://packages.ubuntu.com/groovy/amd64/polybar/download'
+read
