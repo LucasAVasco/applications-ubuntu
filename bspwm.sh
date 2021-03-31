2202c2c8266782f6ef2eac4d803607f1caca5f57
Lucas Vasco <lucas@pop-os.localdomain>
2021-03-31 14:57:53 -0300

    Removed obsession intallation
    Bspwm now uses rofi to shutdown.

--- bspwm.sh
+++ bspwm.sh
@@ -6,8 +6,6 @@ apt install -y bspwm sxhkd pulseaudio pavucontrol rofi brightnessctl
 # Customization
 apt install -y lxappearance nitrogen compton
 
-# Obsession
-apt install -y obsession
 
 # Polybar
 echo 'you need to install Polybar for the Ubuntu groovy packages repository: https://packages.ubuntu.com/groovy/amd64/polybar/download'
