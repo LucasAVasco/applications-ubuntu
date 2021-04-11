a67b053ba7d3559ae0c3829e08863d901b7a159b
Lucas Vasco <none>
2021-04-11 15:30:21 -0300

    Code Highlighter for libreoffice
    New plugin for libreoffice: Code Highlighter

--- document.sh
+++ document.sh
@@ -2,3 +2,6 @@
 
 # Scribus
 apt install -y scribus
+
+# Code highlight for libreoffice
+apt install -y libreoffice-script-provider-python
---
+++ local/document.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# Code highlight for libreoffice 
+pip3 install pygments
+echo 'download code-highlight extension for lobreoffice "https://extensions.libreoffice.org/en/extensions/show/code-highlighter"'
+read
+echo 'Rename the extension to "codehighlighter.oxt" and install'
+read
