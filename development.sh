bc1c005fa17df176c3ee0bee1339d038fe387772
Lucas Vasco <none>
2021-04-27 18:34:51 -0300

    Homebrew installation

--- development.sh
+++ development.sh
@@ -15,6 +15,10 @@ apt install -y fzf silversearcher-ag gawk
 # Bat
 apt install -y bat
 
+# Homebrew
+apt install -y procps file curl
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 # Smath Studio
 echo 'download Smath Stdio in "https://en.smath.com/view/SMathStudio/summary" site'
 read
