e85031e78864146db45339ccf7f0f8be86ef8eca
Lucas Vasco <none>
2021-05-11 15:03:06 -0300

    Lightdm installation
    Installs lightdm, slick-greeter and lightdm-settings.
    
    Also setup slick-greeter as default greeter session.

--- bspwm.sh
+++ bspwm.sh
@@ -9,6 +9,23 @@ apt install -y thunar thunar-archive-plugin thunar-volman
 # Customization
 apt install -y lxappearance nitrogen compton
 
+# Login manager
+apt install -y lightdm
+
+# Greeter
+apt install -y slick-greeter lightdm-settings
+
+# Set the greeter
+GREETER_NAME="slick-greeter"
+CONFIG_FILE="/etc/lightdm/lightdm.conf"
+if [ "$(cat "$CONFIG_FILE" | awk '/greeter-session/')" != '' ]; then
+	# If it already has a defined greeter
+	sed -i s/"greeter-session.*"/"greeter-session=${GREETER_NAME}"/ "$CONFIG_FILE"
+else
+	# If it doesn't have a defined greeter
+	echo "greeter-session=${GREETER_NAME}" >> "$CONFIG_FILE"
+fi
+
 # Screen locker
 apt install -y slim
 
