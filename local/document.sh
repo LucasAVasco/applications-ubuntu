90319923d4e58e944ed157711f2f2da0c406e5c0
LucasAVasco <none>
2021-10-16 18:37:13 -0300

    feat: Zotero installation
    Install zotero (flatpack).
    
    Remember to install Zotero plugin for libreoffice.
    
    ---
    Changes to be committed:
          modified:   local/document.sh

--- local/document.sh
+++ local/document.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 
+# Zotero
+flatpak install -y flathub org.zotero.Zotero
+
+# Zotero libreoffice plugin
+echo 'Install zotero libreoffice plugin at /var/lib/flatpak/app/org.zotero.Zotero/current/active/files/share/zotero/extensions/zoteroOpenOfficeIntegration@zotero.org/install/Zotero_OpenOffice_Integration.oxt'
+
 # Code highlight for libreoffice 
 pip3 install pygments
 echo 'download code-highlight extension for lobreoffice "https://extensions.libreoffice.org/en/extensions/show/code-highlighter"'
