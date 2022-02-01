df4970e9271abce6bf1ed39fa0e83c8d3d155451
LucasAVasco <none>
2022-02-01 18:00:33 -0300

    feat: New Godot installation
    Now Godot engine will be installed by Steam.
    
    Add an advice about this.
    
    ---
    Changes to be committed:
          modified:   local/game_development.sh

--- local/game_development.sh
+++ local/game_development.sh
@@ -5,10 +5,5 @@
 echo 'Execute the another script'
 
 # Godot
-echo download godot engine in \"https://godotengine.org/download/linux\" and press \"Enter\"
+echo Install Godot with Steam
 read
-mkdir -p ~/.local/etc/godot
-mv ~/Downloads/Godot_v* ~/.local/etc/godot/
-unzip ~/.local/etc/godot/Godot_v* -d ~/.local/etc/godot/
-rm ~/.local/etc/godot/Godot_v*.zip
-mv ~/.local/etc/godot/Godot_v* ~/.local/etc/godot/godot
