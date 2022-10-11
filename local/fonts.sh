a299505fa3ef25e607b7e9c4bd546876d93cf209
LucasAVasco <none>
2022-10-11 14:14:11 -0300

    fix: local fonts in '~/.fonts/' folder
    No longer saves fonts in '~/.local/share/fonts/'. Now saves in
    '~/.fonts' directory.
    
    ---
    Changes to be committed:
          modified:   local/fonts.sh

--- local/fonts.sh
+++ local/fonts.sh
@@ -3,8 +3,8 @@
 
 # Install Hack Regular Nerd Font Complete Mono
 HOME_PATH=~
-mkdir -p ~/.local/share/fonts/hack
-curl --location https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf --output ${HOME_PATH}'/.local/share/fonts/hack/Hack Regular Nerd Font Complete Mono.ttf'
+mkdir -p ~/.fonts/nerd
+curl --location https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf --output ${HOME_PATH}'/.fonts/nerd/Hack Regular Nerd Font Complete Mono.ttf'
 
 # Install Noto Sans Nerd Font
-curl --location https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Noto/Sans/complete/Noto%20Sans%20Regular%20Nerd%20Font%20Complete.ttf --output ${HOME_PATH}'/.local/share/fonts/hack/Noto Sans Regular Nerd Font Complete.ttf'
+curl --location https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Noto/Sans/complete/Noto%20Sans%20Regular%20Nerd%20Font%20Complete.ttf --output ${HOME_PATH}'/.fonts/nerd/Noto Sans Regular Nerd Font Complete.ttf'
