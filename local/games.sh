571a9d12194ae49a599108b2ec0b762229e56a91
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2023-07-13 21:31:40 -0300

    feat: install Lutris from Flathub
    Installs the Lutris flatpak from Flathub repositories.
    
    No longer adds a PPA repository to install Lutris.
    
    ---
    Changes to be committed:
          modified:   local/games.sh
          deleted:    root/games.sh

--- local/games.sh
+++ local/games.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 
+# Lutris
+flatpak install -y flathub net.lutris.Lutris
+
 # Update DXVK versions file
 wget https://api.github.com/repos/lutris/dxvk/releases -o $HOME/.local/share/lutris/runtime/dxvk/dxvk_versions.json
