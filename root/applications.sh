d3afbc0c099f52675f271aeef21d7bdff0078508
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2023-08-26 18:18:22 -0300

    Feat: Flatpak installation
    Install Flatpak. Zorin OS automatically installs Flatpak, but Ubuntu
    does not. So now it's required to explicitly install Flatpak
    
    Also add Flathub remote

--- root/applications.sh
+++ root/applications.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+# Flatpak
+apt install -y flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
 # windows
 apt install -y playonlinux
 
