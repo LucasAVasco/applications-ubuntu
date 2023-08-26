750a1af39dc3e455544e854382a6d0514073afc3
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2023-08-26 18:27:50 -0300

    Feat: Remember to install Brave browser
    Add a message to remind you to install Brave browser. Before that, the
    script uses the standard commands provided by Brave to proceed the
    installation. However, this command has changed. For compatibility reasons,
    now there are just a message and a link to Brave browser site.

--- root/browsers.sh
+++ root/browsers.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 
-# Brave
-apt install -y apt-transport-https curl gnupg
-
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
-
-apt update -y
-apt install -y brave-browser
-
 # Tor
 apt install -y torbrowser-launcher
+
+# Brave
+echo -e "\nRemember to install Brave browser: https://brave.com/linux/\n" >> 'root-post-install.txt'
